607e986d1c958d8aa540915308aff7047574825d
oskarwilczynski <oskar.andrzej.wilczynski@gmail.com>
2017-07-23 00:08:27 +0100

    ABOUT finished, started working on SKILLS

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,15 @@ var typed = new Typed('#element', {
     typeSpeed: 25,
     cursorChar: '_'
 });
+
+var typed = new Typed('#skills-confident', {
+    strings: ["Hello...^1000\n\nAnd welcome to my world of web development."],
+    typeSpeed: 25,
+    cursorChar: '_'
+});
+
+var typed = new Typed('#skills-soon', {
+    strings: ["Hello...^1000\n\nAnd welcome to my world of web development."],
+    typeSpeed: 25,
+    cursorChar: '_'
+});
