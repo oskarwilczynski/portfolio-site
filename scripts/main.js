83fb16244815311fd4702dfc88516674a5b0dd62
oskarwilczynski <oskar.andrzej.wilczynski@gmail.com>
2017-07-23 21:16:01 +0100

    Still working on SKILLS section

--- scripts/main.js
+++ scripts/main.js
@@ -5,13 +5,13 @@ var typed = new Typed('#element', {
 });
 
 var typed = new Typed('#skills-confident', {
-    strings: ["Hello...^1000\n\nAnd welcome to my world of web development."],
+    strings: ["<span style='color: #66D9EF'>const</span> skills <span style='color: #F92672'>=</span> {\n&#09;<span style='color: #E6DB74'>'languages'</span> : [<span style='color: #E6DB74'>'HTML'</span>, <span style='color: #E6DB74'>'CSS'</span>, <span style='color: #E6DB74'>'JavaScript'</span>],\n&#09;<span style='color: #E6DB74'>'librariesAndFrameworks'</span> : [<span style='color: #E6DB74'>'jQuery'</span>, <span style='color: #E6DB74'>'Bootstrap'</span>],\n&#09;<span style='color: #E6DB74'>'versionControlSystem'</span> : <span style='color: #E6DB74'>'Git'</span>,\n&#09;<span style='color: #E6DB74'>'textEditors'</span> : [<span style='color: #E6DB74'>'Sublime Text 3'</span>, <span style='color: #E6DB74'>'Brackets'</span>],\n&#09;<span style='color: #E6DB74'>'otherRelatedSkills'</span> : <span style='color: #E6DB74'>'Responsive Web Design'</span>, <span style='color: #E6DB74'>\n&#09;'DOM Manipulation'</span>, <span style='color: #E6DB74'>'AJAX techniques'</span>, <span style='color: #E6DB74'>'Usage of external APIs'</span>]\n}"],
     typeSpeed: 25,
     cursorChar: '_'
 });
 
 var typed = new Typed('#skills-soon', {
-    strings: ["Hello...^1000\n\nAnd welcome to my world of web development."],
+    strings: ["<span style='color: #66D9EF'>const</span> skills <span style='color: #F92672'>=</span> {\n&#09;<span style='color: #E6DB74'>'languages'</span> : [<span style='color: #E6DB74'>'HTML'</span>, <span style='color: #E6DB74'>'CSS'</span>, <span style='color: #E6DB74'>'JavaScript'</span>],\n&#09;<span style='color: #E6DB74'>'librariesAndFrameworks'</span> : [<span style='color: #E6DB74'>'jQuery'</span>, <span style='color: #E6DB74'>'Bootstrap'</span>],\n&#09;<span style='color: #E6DB74'>'versionControlSystem'</span> : <span style='color: #E6DB74'>'Git'</span>,\n&#09;<span style='color: #E6DB74'>'textEditors'</span> : [<span style='color: #E6DB74'>'Sublime Text 3'</span>, <span style='color: #E6DB74'>'Brackets'</span>],\n&#09;<span style='color: #E6DB74'>'otherRelatedSkills'</span> : <span style='color: #E6DB74'>'Responsive Web Design'</span>, <span style='color: #E6DB74'>\n&#09;'DOM Manipulation'</span>, <span style='color: #E6DB74'>'AJAX techniques'</span>, <span style='color: #E6DB74'>'Usage of external APIs'</span>]\n}"],
     typeSpeed: 25,
     cursorChar: '_'
 });
