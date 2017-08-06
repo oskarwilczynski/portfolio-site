093a95d5d9a9359f84c42db3f9ff695cdf337a29
oskarwilczynski <oskar.andrzej.wilczynski@gmail.com>
2017-08-06 22:02:21 +0100

    Done with the button and skills to learn

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-var typed = new Typed('#element', {
+var typed = new Typed('#hello-header', {
     strings: ["Hello...^1000\n\nAnd welcome to my world of web development."],
     typeSpeed: 25,
     cursorChar: '_'
@@ -11,7 +11,7 @@ var typed = new Typed('#skills-confident', {
 });
 
 var typed = new Typed('#skills-soon', {
-    strings: ["<span style='color: #75715e'>// skills I'd like to learn soon</span>\n<span style='color: #66D9EF'>const</span> skills <span style='color: #F92672'>=</span> {\n&#09;<span style='color: #E6DB74'>'languages'</span> : [<span style='color: #E6DB74'>'HTML'</span>, <span style='color: #E6DB74'>'CSS'</span>, <span style='color: #E6DB74'>'JavaScript'</span>],\n&#09;<span style='color: #E6DB74'>'librariesAndFrameworks'</span> : [<span style='color: #E6DB74'>'jQuery'</span>, <span style='color: #E6DB74'>'Bootstrap'</span>],\n&#09;<span style='color: #E6DB74'>'versionControlSystem'</span> : <span style='color: #E6DB74'>'Git'</span>,\n&#09;<span style='color: #E6DB74'>'textEditors'</span> : [<span style='color: #E6DB74'>'Sublime Text 3'</span>, <span style='color: #E6DB74'>'Brackets'</span>],\n&#09;<span style='color: #E6DB74'>'otherRelatedSkills'</span> : [<span style='color: #E6DB74'>\n&#09;&#09;'Responsive Web Design'</span>, <span style='color: #E6DB74'>\n&#09;&#09;'DOM Manipulation'</span>, <span style='color: #E6DB74'>\n&#09;&#09;'AJAX techniques'</span>, <span style='color: #E6DB74'>\n&#09;&#09;'Usage of external APIs'</span>\n&#09;]\n}"],
+    strings: ["<span style='color: #75715e'>// skills I'd like to learn soon</span>\n<span style='color: #66D9EF'>const</span> skills <span style='color: #F92672'>=</span> {\n&#09;<span style='color: #E6DB74'>'librariesAndFrameworks'</span> : [\n&#09;&#09;<span style='color: #E6DB74'>'React (Redux, Flux)'</span>, \n&#09;&#09;<span style='color: #E6DB74'>'Angular'</span>, \n&#09;&#09;<span style='color: #E6DB74'>'Vue.js'</span>\n&#09;],\n&#09;<span style='color: #E6DB74'>'CSSPreprocessors'</span> : <span style='color: #E6DB74'>'SASS'</span>,\n&#09;<span style='color: #E6DB74'>'taskRunners'</span> : <span style='color: #E6DB74'>'gulp'</span>\n}"],
     typeSpeed: 30,
     cursorChar: ''
 });
