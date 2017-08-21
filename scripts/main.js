356ddc8ccd89fff5f49e080721d86a11918a2729
oskarwilczynski <oskar.andrzej.wilczynski@gmail.com>
2017-08-21 22:03:42 +0100

    Scrollspy included, very EXPERIMENTAL changes to navbar

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,30 @@
 $(document).ready(function(){
 
-    var typed = new Typed('#hello-header', {
+    const typed = new Typed('#hello-header', {
         strings: ["Hello...^1000\n\nAnd welcome to my world of web development."],
         typeSpeed: 25,
         cursorChar: '_'
+    });
+
+    let sections = $('a'),
+          nav = $('#navbar-custom'),
+          nav_height = nav.outerHeight();
+     
+    $(window).on('scroll', function () {
+        var cur_pos = $(this).scrollTop();
+     
+        sections.each(function() {
+            var top = $(this).offset().top - nav_height,
+                bottom = top + $(this).outerHeight();
+     
+            if (cur_pos >= top && cur_pos <= bottom) {
+                nav.find('a').removeClass('active');
+                sections.removeClass('active');
+     
+                $(this).addClass('active');
+                nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+            }
+        });
     });
 
     $('a[href*=\\#]').click(function(){
