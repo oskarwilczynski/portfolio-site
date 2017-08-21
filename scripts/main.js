f4641ebf669f3e6d3599aa79f39301fdef4d229b
oskarwilczynski <oskar.andrzej.wilczynski@gmail.com>
2017-08-21 22:05:30 +0100

    Deleted pointless jQuery function

--- scripts/main.js
+++ scripts/main.js
@@ -6,27 +6,6 @@ $(document).ready(function(){
         cursorChar: '_'
     });
 
-    let sections = $('a'),
-          nav = $('#navbar-custom'),
-          nav_height = nav.outerHeight();
-     
-    $(window).on('scroll', function () {
-        var cur_pos = $(this).scrollTop();
-     
-        sections.each(function() {
-            var top = $(this).offset().top - nav_height,
-                bottom = top + $(this).outerHeight();
-     
-            if (cur_pos >= top && cur_pos <= bottom) {
-                nav.find('a').removeClass('active');
-                sections.removeClass('active');
-     
-                $(this).addClass('active');
-                nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
-            }
-        });
-    });
-
     $('a[href*=\\#]').click(function(){
         $('html, body').animate({
             scrollTop: $( $(this).attr('href') ).offset().top
