b9c718efab9afaf0f28b5998492f11989765899d
oskarwilczynski <oskar.andrzej.wilczynski@gmail.com>
2017-08-23 19:34:51 +0100

    Navbar icon design polished, adding Formspree.io to contact form

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,15 @@
 $(document).ready(function(){
 
-    const typed = new Typed('#hello-header', {
+    const typed = new Typed("#hello-header", {
         strings: ["Hello...^1000\n\nAnd welcome to my world of web development."],
         typeSpeed: 25,
-        cursorChar: '_'
+        cursorChar: "_"
     });
 
-    $('a[href*=\\#]').click(function(){
-        $('html, body').animate({
-            scrollTop: $( $(this).attr('href') ).offset().top
+    $("a[href*=\\#]").click(function(){
+        $("html, body").animate({
+            scrollTop: $( $(this).attr("href") ).offset().top
         }, 750);
         return false;
     });
-
 });
